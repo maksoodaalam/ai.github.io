7c6768fcdba07e8fe09ee30e6a22d10e380473e8
maksood aalam <maksoodaalam121@gmail.com>
2020-12-01 07:19:32 +0530

    sa
    s

---
+++ precache-manifest.c26df19507269a7333b9fe1b9dae3cff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d4624edc50857d7c508314cf7724bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc64225b51e68c6b4b8d",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b0477e9ed84dff575214",
+    "url": "./static/css/main.c49f33de.chunk.css"
+  },
+  {
+    "revision": "dc64225b51e68c6b4b8d",
+    "url": "./static/js/2.b760f9b2.chunk.js"
+  },
+  {
+    "revision": "d17cc9771a00e3bfc71a7406a641100c",
+    "url": "./static/js/2.b760f9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0477e9ed84dff575214",
+    "url": "./static/js/main.722fae6d.chunk.js"
+  },
+  {
+    "revision": "ca806589b3b69b013ddb",
+    "url": "./static/js/runtime-main.ff7fde65.js"
+  }
+]);
